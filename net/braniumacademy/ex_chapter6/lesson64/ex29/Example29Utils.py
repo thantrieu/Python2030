9b3b7ca9e764a681cd4e1fc44d9b88535b9f9f64
Trieu Than <trieuthan92pro@gmail.com>
2022-01-14 11:35:02 +0700

    Add exercises 29 l6.4

--- net/braniumacademy/ex_chapter6/lesson64/ex29/Example29Utils.py
+++ net/braniumacademy/ex_chapter6/lesson64/ex29/Example29Utils.py
@@ -12,6 +12,8 @@ def create_student():
     mid = input('Đệm: ').capitalize()
     first = input('Name: ').capitalize()
     birth_date = input('Ngày sinh: ')
+    if len(birth_date) != 10:
+        return None
     major = input('Chuyên ngành: ')
     gpa = float(input('Điểm TB: '))
     full_name = FullName(first, mid, last)
@@ -40,6 +42,35 @@ def read_students_from_file():
             )
             pid = student_reader.readline().strip()
     return mstudents
+
+
+def write_file_student(list_student):
+    with open('STUDENT.DAT', 'w', encoding='UTF-8') as writer:
+        for s in list_student:
+            writer.write(f'{s.person_id}\n')
+            writer.write(f'{s.full_name}\n')
+            writer.write(f'{s.birth_date}\n')
+            writer.write(f'{s.student_id}\n')
+            writer.write(f'{s.gpa}\n')
+            writer.write(f'{s.major}\n')
+
+
+def write_file_subject(list_subject):
+    with open('SUBJECT.DAT', 'w', encoding='UTF-8') as writer:
+        for s in list_subject:
+            writer.write(f'{s.subject_id}\n')
+            writer.write(f'{s.name}\n')
+            writer.write(f'{s.credit}\n')
+
+
+def write_file_register(list_register):
+    with open('REGISTER.DAT', 'w', encoding='UTF-8') as writer:
+        for r in list_register:
+            writer.write(f'{r.register_id}\n')
+            writer.write(f'{r.subject.subject_id}\n')
+            writer.write(f'{r.student.student_id}\n')
+            mformat = '%d/%m/%Y %H:%M:%S'
+            writer.write(f'{r.register_time.strftime(mformat)}\n')
 
 
 def create_subject():
@@ -238,14 +269,18 @@ if __name__ == '__main__':
              '12. Thống kê số lượng sinh viên đăng ký theo từng môn học.\n' \
              '13. Cho biết thông tin bản ghi sớm nhất.\n' \
              '14. Cho biết thông tin bản ghi đăng ký muộn nhất.\n' \
-             '15. Kết thúc chương trình.\n' \
+             '15. Lưu các danh sách vào file.\n' \
+             '16. Kết thúc chương trình.\n' \
              'Xin mời chọn chức năng(1-15): '
     while True:
         choice = int(input(option))
         match choice:
             case 1:
                 new_student = create_student()
-                students.append(new_student)
+                if new_student is not None:
+                    students.append(new_student)
+                else:
+                    print('==> Tạo sinh viên thất bại <==')
             case 2:
                 new_subject = create_subject()
                 subjects.append(new_subject)
@@ -324,7 +359,20 @@ if __name__ == '__main__':
                 else:
                     print('==> Danh sách đăng ký rỗng <==')
             case 15:
+                if len(students) > 0:
+                    write_file_student(students)
+                else:
+                    print('==> Danh sách sinh viên rỗng <==')
+                if len(subjects) > 0:
+                    write_file_subject(subjects)
+                else:
+                    print('==> Danh sách môn học rỗng <==')
+                if len(registers) > 0:
+                    write_file_register(registers)
+                else:
+                    print('==> Danh sách đăng ký rỗng <==')
+            case 16:
                 print('==> Chương trình kết thúc <==')
                 break
             case _:
-                print('==> Lựa chọn không hợp lệ. Vui lòng nhập số 1-15. <==')
+                print('==> Lựa chọn không hợp lệ. Vui lòng nhập số 1-16. <==')
