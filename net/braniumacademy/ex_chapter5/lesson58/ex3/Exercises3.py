43aa009a54a250165f379e7c8ccde85ff32098e8
Trieu Than <trieuthan92pro@gmail.com>
2022-09-02 08:29:55 +0700

    Add update exercises 3

--- net/braniumacademy/ex_chapter5/lesson58/ex3/Exercises3.py
+++ net/braniumacademy/ex_chapter5/lesson58/ex3/Exercises3.py
@@ -28,12 +28,12 @@ class FullName:
 class Employee:
     """Lớp mô tả thông tin nhân viên."""
 
-    def __int__(self, emp_id=None, full_name=None, email=None, phone_number=None, salary=None):
-        self.__emp_id = emp_id
-        self.__full_name = full_name
-        self.__email = email
-        self.__phone_number = phone_number
-        self.__salary = salary
+    def __int__(self):
+        self.__emp_id = ''
+        self.__full_name = None
+        self.__email = ''
+        self.__phone_number = ''
+        self.__salary = 0
 
     @property
     def emp_id(self):
@@ -92,9 +92,329 @@ class Employee:
 class Manager(Employee):
     """Lớp mô tả thông tin người quản lý."""
 
-    def __int__(self, emp_id=None, full_name=None, email=None,
-                phone_number=None, salary=None, role=None, term=None, quater_salary=None):
-        super().__int__(emp_id, full_name, email, phone_number, salary)
-        self.term = term
-        self.role = role
-        self.quater_salary = quater_salary
+    def __int__(self):
+        super().__int__()
+        self.__term = ''
+        self.__role = ''
+        self.__quater_salary = 0
+
+    @property
+    def term(self):
+        return self.__term
+
+    @term.setter
+    def term(self, value):
+        self.__term = value
+
+    @property
+    def role(self):
+        return self.__role
+
+    @role.setter
+    def role(self, value):
+        self.__role = value
+
+    @property
+    def quater_salary(self):
+        return self.__quater_salary
+
+    @quater_salary.setter
+    def quater_salary(self, value):
+        self.__quater_salary = value
+
+    def meeting(self, partner):
+        print(f"Giám đốc {self.full_name} đang họp với đối tác {partner}.")
+
+    def sign(self, document):
+        print(f"Giám đốc đang duyệt văn bản {document}.")
+
+    def calculate_salary(self, working_day):
+        return super().calculate_salary(working_day) + 0.8 * self.quater_salary
+
+
+class Developer(Employee):
+    """Lớp mô tả thông tin của lập trình viên."""
+    def __init__(self):
+        self.__role = ''
+        self.__number_of_planguage = 0
+        self.__number_of_project = 0
+        self.__monthly_kpi = 0
+
+    @property
+    def role(self):
+        return self.__role
+
+    @role.setter
+    def role(self, value):
+        self.__role = value
+
+    @property
+    def num_of_language(self):
+        return self.__number_of_planguage
+
+    @num_of_language.setter
+    def num_of_language(self, value):
+        self.__number_of_planguage = value
+
+    @property
+    def num_of_project(self):
+        return self.__number_of_project
+
+    @num_of_project.setter
+    def num_of_project(self, value):
+        self.__number_of_project = value
+
+    @property
+    def monthly_kpi(self):
+        return self.__monthly_kpi
+
+    @monthly_kpi.setter
+    def monthly_kpi(self, value):
+        self.__monthly_kpi = value
+
+    def receive_task(self, task):
+        print(f"Lập trình viên {self.full_name} đã nhận task {task}.")
+
+    def solve(self, task):
+        print(f"Lập trình viên {self.full_name} đang thực hiện task {task}.")
+
+    def fix(self, task):
+        print(f"Lập trình viên {self.full_name} đang fix bugs task {task}.")
+
+    def report(self, task, manager):
+        print(f"Lập trình viên {self.full_name} đang làm báo cáo task {task} cho quản lý {manager.full_name}.")
+
+
+class Tester(Employee):
+    def __init__(self):
+        self.__role = ''
+        self.__tools = []
+        self.__error_found = 0
+        self.__number_of_testcase = 0
+
+    @property
+    def role(self):
+        return self.__role
+
+    @property
+    def tools(self):
+        return self.__tools
+
+    @property
+    def error_found(self):
+        return self.__error_found
+
+    @property
+    def number_of_testcase(self):
+        return self.__number_of_testcase
+
+    @role.setter
+    def role(self, value):
+        self.__role = value
+
+    @tools.setter
+    def tools(self, value):
+        self.__tools = value
+
+    @error_found.setter
+    def error_found(self, value):
+        self.__error_found = value
+
+    @property
+    def number_of_testcase(self, value):
+        self.__number_of_testcase = value
+
+    def receive_project(self, project_name):
+        print(f"Tester {self.full_name} đã nhận dự án {project_name}.")
+
+    def prepare_testcase(self, project_name):
+        print(f"Tester {self.full_name} đang chuẩn bị testcase cho dự án {project_name}.")
+
+    def test(self, function):
+        print(f"Tester {self.full_name} đang test chức năng {function}.")
+
+    def report(self, error_name, dev_name):
+        print(f"Tester {self.full_name} đã report {error_name} đến dev {dev_name}.")
+
+
+class Task:
+    """Lớp mô tả công việc."""
+    AUTO_ID = 1000
+
+    def __init__(self):
+        self.__task_id = ''
+        self.__task_name = ''
+        self.__estimated_time = 0
+
+    @property
+    def task_id(self):
+        return self.__task_id
+
+    @task_id.setter
+    def task_id(self, value):
+        if value is None or value == '':
+            self.__task_id = f"T{Task.AUTO_ID + 1}"
+            Task.AUTO_ID += 1  # mã công việc tự tăng lên 1
+        else:
+            self.__task_id = value
+
+    @property
+    def task_name(self):
+        return self.__task_name
+
+    @task_name.setter
+    def task_name(self, value):
+        self.__task_name = value
+
+    @property
+    def estimated_time(self):
+        return self.__estimated_time
+
+    @estimated_time.setter
+    def estimated_time(self, value):
+        self.__estimated_time = value
+
+
+class Assignment:
+    """Lớp mô tả bảng phân công công việc."""
+    AUTO_ID = 1000
+
+    def __init__(self):
+        self.__ass_id = 0
+        self.__staff = None
+        self.__task = None
+        self.__start_time = None
+        self.__deadline = None
+        self.__result = ''
+    @property
+    def ass_id(self):
+        return self.__ass_id
+
+    @ass_id.setter
+    def ass_id(self, value):
+        if value is None or value == 0:
+            self.__ass_id = Assignment.AUTO_ID + 1
+            Assignment.AUTO_ID += 1
+        else:
+            self.__ass_id = value
+
+    @property
+    def staff(self):
+        return self.__staff
+
+    @staff.setter
+    def staff(self, value):
+        self.__staff = value
+
+    @property
+    def task(self):
+        return self.__staff
+
+    @task.setter
+    def task(self, value):
+        self.__task = value
+
+    @property
+    def start_time(self):
+        return self.__start_time
+
+    @start_time.setter
+    def start_time(self, value):
+        self.__start_time = value
+
+    @property
+    def deadline(self):
+        return self.deadline
+
+    @deadline.setter
+    def deadline(self, value):
+        self.__deadline = value
+
+    @property
+    def result(self):
+        return self.result
+
+    @result.setter
+    def result(self, value):
+        self.__result = value
+
+class Payroll:
+    AUTO_ID = 1000
+
+    def __init__(self):
+        self.__payroll_id = 0
+        self.__assignments = []
+        self.__staff = None
+        self.__total_task = 0
+        self.__total_finished = 0
+        self.__total_unfinished = 0
+        self.__total_penalty_fee = 0
+        self.__received_salary = 0
+
+    @property
+    def payroll_id(self):
+        return self.__payroll_id
+
+    @payroll_id.setter
+    def payroll_id(self, value):
+        if va is None or value == 0:
+            self.__payroll_id = Payroll.AUTO_ID + 1
+            Payroll.AUTO_ID += 1
+        else:
+            self.__payroll_id = value
+
+    @property
+    def assignments(self):
+        return self.__assignments
+
+    @assignments.setter
+    def assignments(self, value):
+        self.__assignments = value
+
+    @property
+    def staff(self):
+        return self.__staff
+
+    @staff.setter
+    def staff(self, value):
+        self.__staff = value
+
+    @property
+    def total_task(self):
+        return self.__total_task
+
+    @total_task.setter
+    def total_task(self, value):
+        self.__total_task = value
+
+    @property
+    def total_finished(self):
+        return self.__total_finished
+
+    @total_finished.setter
+    def total_finished(self, value):
+        self.__total_finished = value
+
+    @property
+    def total_unfinished(self):
+        return self.__total_unfinished
+
+    @total_unfinished.setter
+    def total_unfinished(self, value):
+        self.__total_unfinished = value
+
+    @property
+    def total_penalty_fee(self):
+        return self.__total_penalty_fee
+
+    @total_penalty_fee.setter
+    def total_penalty_fee(self, value):
+        self.__total_penalty_fee = value
+
+    @property
+    def received_salary(self):
+        return self.__received_salary
+
+    @received_salary.setter
+    def received_salary(self, value):
+        self.__received_salary = value
