724d7660b5fce1e0f4b0feb4d63f4e8b5e93b3de
Trieu Than <trieuthan92pro@gmail.com>
2022-09-12 17:20:28 +0700

    Add exception filter for exercises

--- net/braniumacademy/ex_chapter7/lesson74/ex3/filters.py
+++ net/braniumacademy/ex_chapter7/lesson74/ex3/filters.py
@@ -30,42 +30,42 @@ def is_birth_date_valid(birth_date):
 def is_gpa_valid(gpa_str):
     """Phương thức kiểm tra xem giá trị gpa có hợp lệ không."""
     if len(gpa_str.strip()) == 0:
-        return False
+        raise GpaError(gpa_str, 'Điểm gpa không được để trống')
     pattern = '^\\d{1}.\\d{1,2}$'  # chi tiết bạn vui lòng xem bài học 9.1
     if re.match(pattern, gpa_str) and 0.0 <= float(gpa_str) <= 4.0:
         return True
     else:
-        return False
+        raise GpaError(gpa_str, 'Điểm gpa không hợp lệ')
 
 
 def is_credit_valid(credit_str):
     """Phương thức kiểm tra số tín chỉ có hợp lệ không."""
     if len(credit_str.strip()) == 0:
-        return False
+        raise CreditError(credit_str, 'Số tín chỉ không thể bỏ trống')
     if credit_str.isdigit() and 1 <= int(credit_str) <= 15:
         return True
-    return False
+    raise CreditError(credit_str, 'Số tín chỉ không hợp lệ')
 
 
 def is_salary_valid(salary_str):
     """Phương thức kiểm tra mức lương có hợp lệ không."""
     if salary_str.strip().isdigit() and int(salary_str) > 0:  # nếu là các con số
         return True
-    return False
+    raise SalaryError(salary_str, 'Mức lương không hợp lệ')
 
 
 def is_subject_id_valid(subject_id):
     """Phương thức kiểm tra mã môn học có hợp lệ không."""
     if subject_id.isdigit() and int(subject_id) >= 1000:
         return True
-    return False
+    raise SubjectIdError(subject_id, 'Mã môn học không hợp lệ')
 
 
 def is_transcript_id_valid(transcript_id):
     """Phương thức kiểm tra mã bảng điểm có hợp lệ không."""
     if transcript_id.isdigit() and int(transcript_id) >= 100:
         return True
-    return False
+    raise TranscriptIdError(transcript_id, 'Mã bảng đăng ký không hơp lệ')
 
 
 def is_student_id_valid(student_id):
@@ -74,7 +74,7 @@ def is_student_id_valid(student_id):
     if len(student_id) == 6 and re.match(pattern, student_id, re.IGNORECASE):
         if int(student_id[2:]) >= 1000:
             return True
-    return False
+    raise StudentIdError(student_id, 'Mã sinh viên không hợp lệ')
 
 
 def is_teacher_id_valid(teacher_id):
@@ -83,7 +83,7 @@ def is_teacher_id_valid(teacher_id):
     if len(teacher_id) == 6 and re.match(pattern, teacher_id, re.IGNORECASE):
         if int(teacher_id[2:]) >= 1001:
             return True
-    return False
+    raise TeacherIdError(teacher_id, 'Mã giảng viên không hợp lệ')
 
 
 def is_course_id_valid(course_id):
@@ -92,4 +92,4 @@ def is_course_id_valid(course_id):
     if len(course_id) == 4 and re.match(pattern, course_id, re.IGNORECASE):
         if int(course_id[1:]) >= 100:
             return True
-    return False
+    raise CourseIdError(course_id, 'Mã khóa học không hợp lệ')
