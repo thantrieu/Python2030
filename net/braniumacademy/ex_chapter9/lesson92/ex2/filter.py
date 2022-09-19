2aff31399da22b8c68ade55a400ab275dec85918
Trieu Than <trieuthan92pro@gmail.com>
2022-09-19 11:35:06 +0700

    Add update exercises 2 lesson 9.2

---
+++ net/braniumacademy/ex_chapter9/lesson92/ex2/filter.py
@@ -0,0 +1,96 @@
+import re
+from exceptions import *
+
+
+def is_name_valid(name):
+    """
+        Phương thức kiểm tra họ tên có hợp lệ không.
+        Giả định họ tên hợp lệ chỉ chứa kí tự chữ cái và khoảng trắng.
+    """
+    if len(name.strip()) < 2:
+        raise FullNameError(name, 'Họ và tên quá ngắn')
+    if len(name.strip()) > 40:
+        raise FullNameError(name, 'Họ và tên quá dài')
+    elif re.match(r'\W+|.*[0-9_]+.*', name, re.UNICODE):
+        raise FullNameError(name, f'Họ và tên chứa kí tự không hợp lệ')
+    return True
+
+
+def is_subject_name_valid(name):
+    """
+        Phương thức kiểm tra tên môn học có hợp lệ không.
+        Giả định họ tên hợp lệ chỉ chứa kí tự chữ cái, số và khoảng trắng.
+    """
+    if len(name.strip()) < 2:
+        raise SubjectNameError(name, 'Tên môn học quá ngắn')
+    elif len(name.strip()) > 50:
+        raise SubjectNameError(name, 'Tên môn học quá dài')
+    elif not re.match(r'\w+|.*[+#.].*', name):
+        raise SubjectNameError(name, 'Tên môn học không hợp lệ')
+    return True
+
+
+def is_person_id_valid(person_id):
+    """
+        Phương thức kiểm tra xem mã số CMND/CCCD có hợp lệ không.
+        Mã hợp lệ nếu chỉ chứa chữ cái và số.
+    """
+    if len(person_id.strip()) == 0:  # nếu chuỗi rỗng -> id ko hợp lệ
+        return False
+    if not person_id.isalnum():  # nếu số cmnd/cccd có kí tự khác số và chữ cái
+        return False  # mã k hợp lệ
+    return True
+
+
+def is_birth_date_valid(birth_date):
+    """Phương thức kiểm tra xem ngày sinh có hợp lệ không."""
+    if len(birth_date.strip()) != 10:
+        raise BirthDateError(birth_date, 'Ngày sinh không đúng định dạng')
+    pattern = '^\\d{2}/\\d{2}/\\d{4}$'  # chi tiết bạn vui lòng xem bài học 9.1
+    if re.match(pattern, birth_date.strip()):
+        return True
+    raise BirthDateError(birth_date, 'Ngày sinh không hợp lệ')
+
+
+def is_gpa_valid(gpa_str):
+    """Phương thức kiểm tra xem giá trị gpa có hợp lệ không."""
+    if len(gpa_str.strip()) == 0:
+        raise GpaError(gpa_str, 'Điểm gpa không được để trống')
+    pattern = '^\\d.\\d{1,2}|\\d$'  # chi tiết bạn vui lòng xem bài học 9.1
+    if re.match(pattern, gpa_str):
+        return True
+    else:
+        raise GpaError(gpa_str, 'Điểm gpa không hợp lệ')
+
+
+def is_student_id_valid(student_id):
+    """Phương thức kiểm tra mã sinh viên cho trước có hợp lệ không."""
+    if len(student_id) == 0:
+        raise StudentIdError(student_id, 'Mã sinh viên rỗng')
+    pattern = '^SV\\d{4}$'
+    if re.match(pattern, student_id, re.IGNORECASE) and int(student_id[2:]) >= 1000:
+        return True
+    raise StudentIdError(student_id, 'Mã sinh viên không hợp lệ')
+
+
+def is_credit_valid(credit_str):
+    """Phương thức kiểm tra số tín chỉ có hợp lệ không."""
+    if len(credit_str.strip()) == 0:
+        raise CreditError(credit_str, 'Số tín chỉ không thể để trống')
+    if re.match(r'\d+', credit_str):
+        return True
+    raise CreditError(credit_str, 'Số tín chỉ không hợp lệ')
+
+
+def is_subject_id_valid(value):
+    """Phương thức kiểm tra xem mã môn học có hợp lệ không."""
+    if re.match('^\\d{4}$', value) and 1000 <= int(value) < 10000:
+        return True
+    raise SubjectIdError(value, 'Mã môn học không hợp lệ')
+
+
+def is_register_id_valid(value):
+    """Phương thức kiểm tra xem mã đăng ký có hợp lệ không."""
+    if re.match('\\d{4}', value) and int(value) >= 1000:
+        return True
+    raise RegisterIdError(value, 'Mã đăng ký không hợp lệ')
---
+++ net/braniumacademy/ex_chapter9/lesson92/ex2/student.py
@@ -0,0 +1,78 @@
+from person import Person
+from filter import *
+
+
+class Student(Person):
+    """Lớp mô tả thông tin sinh viên"""
+    AUTO_ID = 1000
+
+    def __init__(self, pid=None, full_name='',
+                 birth_date='', sid=None, gpa='', major=''):
+        super().__init__(pid, full_name, birth_date)
+        self.student_id = sid
+        self.gpa = gpa
+        self.major = major
+
+    @property
+    def student_id(self):
+        return self.__student_id
+
+    @student_id.setter
+    def student_id(self, value):
+        if value is None:
+            self.__student_id = f'SV{Student.AUTO_ID}'
+            Student.AUTO_ID += 1
+        else:
+            try:
+                if is_student_id_valid(value):
+                    self.__student_id = value
+            except StudentIdError as e:
+                print(e)
+                self.__student_id = None
+
+    @property
+    def gpa(self):
+        return self.__gpa
+
+    @gpa.setter
+    def gpa(self, value):
+        try:
+            if is_gpa_valid(value):
+                self.__gpa = float(value)
+        except GpaError as e:
+            print(e)
+            self.__gpa = 0
+
+    @property
+    def major(self):
+        return self.__major
+
+    @major.setter
+    def major(self, value):
+        self.__major = value
+
+    def show_info(self):
+        super().show_info()
+        print(f'Mã sinh viên: {self.student_id}')
+        print(f'Điểm Gpa: {self.gpa}')
+        print(f'Chuyên ngành: {self.major}')
+
+    def do_exam(self, subject):
+        print(f'Sinh viên {self.student_id} đang làm bài thi môn {subject}')
+
+    def register(self, subject):
+        print(f'Sinh viên {self.student_id} đang đăng ký môn học {subject}')
+
+    def __str__(self):
+        return f'{self.person_id:15}{self.full_name.__str__():30}' \
+               f'{self.birth_date.strftime("%d/%m/%Y"):20}' \
+               f'{self.student_id:15}{self.major:15}' \
+               f'{self.gpa:<15}'
+
+    def file_output_format(self):
+        return f'{self.person_id}\n' \
+               f'{self.full_name}\n' \
+               f'{self.birth_date}\n' \
+               f'{self.student_id}\n' \
+               f'{self.gpa}\n' \
+               f'{self.major}\n'
